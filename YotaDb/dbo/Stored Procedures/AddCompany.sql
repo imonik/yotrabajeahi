3f62d59380b458d54e645c97d8b1e8443636bb6c
Fernando Mendoza <fcmendoza02@gmail.com>
2012-12-16 18:45:33 -0600

    Added database project with intial db objects.

---
+++ YotaDb/dbo/Stored Procedures/AddCompany.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[AddCompany]
+(
+	@Name varchar(500)
+)
+AS
+BEGIN
+	INSERT INTO Companies (Name)
+	VALUES(@Name)
+END
---
+++ YotaDb/dbo/Tables/Companies.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Companies]
+(
+	 ID int NOT NULL CONSTRAINT [PK_Companies] PRIMARY KEY
+	,Name nvarchar(500) NOT NULL
+	,CreatedOn datetime2 NOT NULL CONSTRAINT [DF_Companies_CreatedOn] DEFAULT (GETUTCDATE())
+)
