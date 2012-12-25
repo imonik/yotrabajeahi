34213ce365a9c8a9272da0580b72dd0399aeca4b
Fernando Mendoza <fcmendoza02@gmail.com>
2012-12-24 18:55:35 -0600

    Added new Services project to solution and (incomplete) implemented addition to comments.

---
+++ Services/Repositories/CompanyRepository.cs
@@ -0,0 +1,21 @@
+﻿using Services.Interfaces;
+using Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;  
+using System.Threading.Tasks;
+using Yota.DataAccess;
+
+namespace Services.Repositories
+{
+    public class CompanyRepository : ICompanyRepository
+    {
+        public void AddComment(Comment comment)
+        {
+            _db.AddComment(comment.Content); //TODO: complete this method to include the rest of the information necessary to add a comment.
+        }
+
+        YotaDB _db = new YotaDB();
+    }
+}
---
+++ Services/Subsonic/StoredProcedures.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+  
+using System;
+using SubSonic;
+using SubSonic.Schema;
+using SubSonic.DataProviders;
+using System.Data;
+
+namespace Yota.DataAccess{
+	internal partial class YotaDB{
+
+		public StoredProcedure AddComment(string Comment){
+			StoredProcedure sp=new StoredProcedure("AddComment",this.Provider);
+			sp.Command.AddParameter("Comment",Comment,DbType.AnsiString,ParameterDirection.Input);
+			return sp;
+		}
+		public StoredProcedure AddCompany(string Name){
+			StoredProcedure sp=new StoredProcedure("AddCompany",this.Provider);
+			sp.Command.AddParameter("Name",Name,DbType.AnsiString,ParameterDirection.Input);
+			return sp;
+		}
+	}
+}
+ 
