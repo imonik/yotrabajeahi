bc146b5611b4a4d5dab2958f0022eb9aeea385bf
Fernando Mendoza <fcmendoza02@gmail.com>
2013-01-13 18:31:15 -0600

    Added sample (dummy) companies listing.

---
+++ Yotah/Controllers/CompaniesController.cs
@@ -0,0 +1,28 @@
+﻿using MvcApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class CompaniesController : Controller
+    {
+        //
+        // GET: /Companies/
+
+        public ActionResult Index()
+        {
+            var companies = new List<CompanyViewModel>();
+
+            companies.Add(new CompanyViewModel { Name = "Epicor Software Co. Ltd.", GeneralScore = 8.5m, CommentsCount = 15, ViewsCount = 53, ModifiedOn = new DateTime(2013, 1, 13, 15, 30, 20), ModifiedBy = "monik" });
+            companies.Add(new CompanyViewModel { Name = "Inflection Pont Systems", GeneralScore = 9.1m, CommentsCount = 52, ViewsCount = 47, ModifiedOn = new DateTime(2013, 1, 13, 18, 15, 20), ModifiedBy = "monik" });
+            companies.Add(new CompanyViewModel { Name = "Sieena software", GeneralScore = 8.9m, CommentsCount = 10, ViewsCount = 27, ModifiedOn = new DateTime(2013, 1, 13, 15, 30, 20), ModifiedBy = "fer" });
+            companies.Add(new CompanyViewModel { Name = "Venice Consulting Group Ltd.", GeneralScore = 9.4m, CommentsCount = 35, ViewsCount = 129, ModifiedOn = new DateTime(2013, 1, 13, 15, 30, 20), ModifiedBy = "someone" });
+
+            return View(companies);
+        }
+
+    }
+}
---
+++ Yotah/Models/CompanyViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class CompanyViewModel
+    {
+        public string Name { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public string ModifiedBy { get; set; }
+        public decimal GeneralScore { get; set; }
+        public int CommentsCount { get; set; }
+        public int ViewsCount { get; set; }
+    }
+}
